4869532d5d60ca488c4c4aafba3219c46d0007e5
39M <xwszc7@gmail.com>
2019-01-27 11:01:01 +0800

    init scene

---
+++ Assets/Scripts/Init.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Init : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+            SceneManager.LoadScene("Game");
+    }
+}
