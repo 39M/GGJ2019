8b2200c8f5978909940f5b997242f4b1e4760484
39M <xwszc7@gmail.com>
2019-01-27 13:17:47 +0800

    init 场景

--- Assets/Scripts/Init.cs
+++ Assets/Scripts/Init.cs
@@ -1,11 +1,25 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Init : MonoBehaviour
 {
+    public GameObject black;
+
+    bool start = false;
     void Update()
     {
+        if (start)
+            return;
+
         if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
-            SceneManager.LoadScene("Game");
+        {
+            start = true;
+            black.SetActive(true);
+            black.transform.DOLocalMoveZ(1, 0.25f).OnComplete(() =>
+            {
+                SceneManager.LoadScene("Game");
+            });
+        }
     }
 }
